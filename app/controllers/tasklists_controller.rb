fc64f19a7768d8bf97f9f5fab11ae9b556498885
Takashi_Furuya <takashi.furuya.0@gmail.com>
2017-04-17 07:31:12 +0000

    make view-files

---
+++ app/controllers/tasklists_controller.rb
@@ -0,0 +1,30 @@
+class TasklistsController < ApplicationController
+  def index
+    @
+  end
+
+  def show
+		@
+  end
+
+	def new
+		@
+	end
+
+	def create
+		@
+	end
+
+	def edit
+		@
+	end
+
+	def update
+		@
+	end
+
+	private
+	# Strong 
+	def content_params
+	end
+end
